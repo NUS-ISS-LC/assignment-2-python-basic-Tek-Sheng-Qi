e0ede5dd3491e3df25de6c7f04bd1bdbb987ca37
Tek Sheng Qi <146525821+Tek-Sheng-Qi@users.noreply.github.com>
2025-11-09 07:57:32 +0000

    completed python assignment 2

--- ex1.py
+++ ex1.py
@@ -1,3 +1,20 @@
+#s is the array nums
+#n is the target
 def find(s, n):
-# write your implementation here
-    return None
+    rounds = len(s) - 1
+    first_index = 0
+    second_index = 1
+    for i in range(rounds): #0, 1, 2
+        for j in range(1+i,len(s)): #1,2,3
+            sum = s[i] + s[j]
+            if sum == n:
+                first_index = i
+                second_index = j
+                break
+    return [first_index, second_index]        
+
+nums = [2,7,11,15]
+target = 9 
+print(find(nums,target))
+    
+
